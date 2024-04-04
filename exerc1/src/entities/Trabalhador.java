62685b5ae4bd6138b546460fcfb072696179517b
Sandro G Win <sandrogp13@gmail.com>
2024-04-04 18:47:27 -0300

    upload exerc 1

---
+++ exerc1/src/entities/Trabalhador.java
@@ -0,0 +1,87 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.NivelTrabalhador;
+
+public class Trabalhador {
+    private String nome;
+    private NivelTrabalhador nivel;
+    private Double salarioBase;
+    
+    private Departamento departamento;
+    private List<HorasContrato> contratos = new ArrayList<>();
+
+    public Trabalhador() {
+    }
+
+    public Trabalhador(String nome, NivelTrabalhador nivel, Double salarioBase, Departamento departamento) {
+        this.nome = nome;
+        this.nivel = nivel;
+        this.salarioBase = salarioBase;
+        this.departamento = departamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public NivelTrabalhador getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(NivelTrabalhador nivel) {
+        this.nivel = nivel;
+    }
+
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(Double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<HorasContrato> getContratos() {
+        return contratos;
+    }
+
+    // metodos
+    public void addContrato(HorasContrato contrato) {
+        contratos.add(contrato);
+    }
+
+    public void removeContrato(HorasContrato contrato) {
+        contratos.remove(contrato);
+    }
+
+    public Double renda(int ano, int mes) {
+        double soma = salarioBase;
+        Calendar cal = Calendar.getInstance();
+        for(HorasContrato c : contratos) {
+            cal.setTime(c.getData());
+            int c_ano = cal.get(Calendar.YEAR);
+            int c_mes = 1 + cal.get(Calendar.MONTH);
+            if(ano == c_ano && mes == c_mes) {
+                soma += c.valorTotal();
+            }
+        }
+
+        return soma;
+    }
+
+}
---
+++ exerc1/src/entities/enums/NivelTrabalhador.java
@@ -0,0 +1,7 @@
+package entities.enums;
+
+public enum NivelTrabalhador {
+    JUNIOR,
+    NIVEL_MEDIO,
+    SENIOR;
+}
